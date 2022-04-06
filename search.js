179b04e3e05ddb74b9305c0148a71a7c3ef4bb0a
I-keep-trying <26046173+I-keep-trying@users.noreply.github.com>
2022-04-06 15:28:46 -0400

    data directory

--- search.js
+++ search.js
@@ -1,20 +1,58 @@
 const fs = require('fs')
-const allData = require('./data1.json')
+const allData = require('./data.json')
 const names = require('./countryNames.json')
 
+const createNew = (name) => {
+  try {
+    if (fs.existsSync(`./data/${name}.json`)) {
+      return
+    } else {
+      fs.writeFile(`./data/${name}.json`, '[]', (error, data) => {
+        if (error) {
+          console.log('createNew error: ', error)
+          return
+        }
+      })
+    }
+  } catch (err) {
+    console.log('fs.existsSync error: ', err)
+  }
+}
+
 const findCountries = () =>
-  names.map((name) => {
-    return allData.map((item) => {
-        const findName = item.Government['Country name']["conventional short form"].text
-     if (name === findName) {
-        console.log('item.Government', findName)}
-      return item
-    
-    })
+  allData.map((item) => {
+    if (item.Government['Country name'] !== undefined) {
+      const findName =
+        item.Government['Country name']['conventional short form'].text
+      names.map((name) => {
+        createNew(name)
+        if (findName === name) {
+          console.log('found item', name)
+          fs.readFile(`./data/${name}.json`, (error, data) => {
+            if (error) {
+              console.log(error)
+              return
+            }
+            fs.writeFile(
+                `./data/${name}.json`,
+                JSON.stringify(item, null, 2),
+                (err) => {
+                  if (err) {
+                    console.log('Failed to write data')
+                    return
+                  }
+                  console.log('Updated data file successfully')
+                }
+              )
+          })
+          return item
+        }
+      })
+      return
+    }
+    console.log('item undefined')
+    return
   })
 
-findCountries() 
+findCountries()
 
-// Algeria
-// Angola
-// Botswana
